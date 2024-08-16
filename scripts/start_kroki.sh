fe11bfe2fd8beb8d54fbccd2291b5690bdf2b03f
Randall White <randall.white@login-02.clusternet>
2024-08-16 15:34:50 -0700

    blah end of day

--- scripts/start_kroki.sh
+++ scripts/start_kroki.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-coproc KROKI_SERV_INST { java -DKROKI_LISTEN=127.0.0.1:${MAIN_KROKI_PORT} -jar /usr/local/kroki/kroki-server.jar &> /tmp/kroki_server.log }
+coproc KROKI_SERV_INST java -DKROKI_LISTEN=127.0.0.1:${MAIN_KROKI_PORT} -jar /usr/local/kroki/kroki-server.jar &> /tmp/kroki_server.log
+
+echo ${MAIN_KROKI_PORT}
+
+export KROKI_SERV_INST
 
 sleep 4
 
--- scripts/stop_kroki.sh
+++ scripts/stop_kroki.sh
@@ -1,4 +1,4 @@
 #!/bin/bash 
 
-kill -5 $KROKI_SERV_INST_PID
+kill $KROKI_SERV_INST_PID
 
